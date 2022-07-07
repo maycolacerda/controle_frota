f418f859224ce691c4fa6783c858e08a355a12e3
maycolacerda <maycolacerda@gmail.com>
2022-07-07 02:15:13 -0400

    todo

--- controle_frota_api_rest/models/manutencao.go
+++ controle_frota_api_rest/models/manutencao.go
@@ -5,6 +5,16 @@ import (
 	"gorm.io/gorm"
 )
 
+// // Manutencao is a struct that represents a maintenance record.
+// // Manutencao
+// @property  - Model: This is a struct that contains the following fields: ID, CreatedAt, UpdatedAt,
+// DeletedAt.
+// @property {uint} Id - The primary key of the table.
+// @property {uint} IdVeiculo - The vehicle ID
+// @property {string} DataInicio - Start date
+// @property {string} DataFim - Date of the end of the maintenance
+// @property {string} Descricao - Description of the maintenance
+// @property {bool} Concluido - true or false
 type Manutencao struct {
 	gorm.Model
 	Id         uint   `json:"id_manutencao" gorm:"primary_key"`
@@ -15,6 +25,7 @@ type Manutencao struct {
 	Concluido  bool   `json:"concluido"`
 }
 
+// It validates the struct Manutencao using the validator package
 func ValidacaoManutencao(manutencao *Manutencao) error {
 
 	validate := validator.New()
